b2320e74cbc0b0ad8fceaa20234f6ef02bc4cfad
chandan yadav <chandanky.23@gmail.com>
2020-12-22 19:58:06 +0100

    Adding a new route in auction service to get an auction by id and expanding the iam service to hanldle queries for dynamodb

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -8,33 +8,39 @@ import createError from "http-errors"
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
 /**
- * This is a Lambda Fucntion to get all the auctions using REST API
+ * This is a Lambda Fucntion to get an auction by id using REST API
  * @param {*} event contains all the params, body, and header data passed while making the api call
  * @param {*} context stores the metadata of the execution of the lambda function
  *
  * The return of the lambda function needs to be a string, if an object is returned then it first needs to be stringified, else it will throw an error.
  */
-async function getAuctions(event, context) {
-  let auctions
+async function getAuction(event, context) {
+  let auction
+  const { id } = event.pathParameters
 
   try {
-    const result = await dynamoDB.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME
+    const result = await dynamoDB.get({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Key: { id },
     }).promise()
 
-    auctions = result.Items
-  }catch(err) {
-    console.error(err)
+    auction = result.Item
+  } catch (error) {
+    console.error(error)
     throw new createError.InternalServerError(error)
+  }
+
+  if(!auction) {
+    throw new createError.NotFound(`Auction with ID: "${id}" not found!`)
   }
 
   return {
     statusCode: 200,
-    body: JSON.stringify(auctions),
+    body: JSON.stringify(auction),
   }
 }
 
-export const handler = middy(getAuctions)
+export const handler = middy(getAuction)
   .use(httpJsonBodyParser())
   .use(httpEventNormalizer())
   .use(httpErrorHandler())
