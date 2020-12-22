12eaafd03902d75684eb33b1b6d8510f69b2adb4
chandan yadav <chandanky.23@gmail.com>
2020-12-22 19:15:49 +0100

    Adding a new lamda function in the auctions service to get the list of all the auctions from dynamodb table AuctionsTable

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from "uuid"
 import AWS from "aws-sdk"
 import middy from "@middy/core"
 import httpJsonBodyParser from "@middy/http-json-body-parser"
@@ -9,42 +8,33 @@ import createError from "http-errors"
 const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
 /**
- * This is a Lambda Fucntion to create an aution using REST API
+ * This is a Lambda Fucntion to get all the auctions using REST API
  * @param {*} event contains all the params, body, and header data passed while making the api call
  * @param {*} context stores the metadata of the execution of the lambda function
  *
  * The return of the lambda function needs to be a string, if an object is returned then it first needs to be stringified, else it will throw an error.
  */
-async function createAuction(event, context) {
-  const { title } = event.body
-  const now = new Date()
-
-  const auction = {
-    id: uuid(),
-    title,
-    status: "OPEN",
-    createdAt: now.toISOString(),
-  }
+async function getAuctions(event, context) {
+  let auctions
 
   try {
-    await dynamoDB
-      .put({
-        TableName: process.env.AUCTIONS_TABLE_NAME,
-        Item: auction,
-      })
-      .promise()
-  } catch (err) {
+    const result = await dynamoDB.scan({
+      TableName: process.env.AUCTIONS_TABLE_NAME
+    }).promise()
+
+    auctions = result.Items
+  }catch(err) {
     console.error(err)
     throw new createError.InternalServerError(error)
   }
 
   return {
     statusCode: 201,
-    body: JSON.stringify(auction),
+    body: JSON.stringify(auctions),
   }
 }
 
-export const handler = middy(createAuction)
+export const handler = middy(getAuctions)
   .use(httpJsonBodyParser())
   .use(httpEventNormalizer())
   .use(httpErrorHandler())
