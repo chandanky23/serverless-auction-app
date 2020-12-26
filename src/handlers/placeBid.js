eff73669354a6a973a1ee986b48bb0f3742467f8
chandan yadav <chandanky.23@gmail.com>
2020-12-26 20:39:40 +0100

    Adding a helper function to close a bid, updating placeBid Lambda function to forbid price update for a closed auction and throwing error message

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -18,6 +18,10 @@ async function placeBid(event, context) {
 
   const auction = await getAuctionById(id)
 
+  if(auction.status !== 'OPEN') {
+    throw new createError.Forbidden('You cannot bid on a CLOSED auction')
+  }
+
   if(amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}`)
   }
--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,8 +1,21 @@
+import craeteError from "http-errors"
 import { getEndedAuctions } from "../lib/getEndedAuctions"
+import { closeAuction } from "../lib/closeAuction"
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions()
-  console.log(auctionsToClose)
+  try {
+    const auctionsToClose = await getEndedAuctions()
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    )
+    await Promise.all(closePromises)
+    return {
+      closed: closePromises.length,
+    }
+  } catch (err) {
+    console.error(err)
+    throw new craeteError.InternalServerError(err)
+  }
 }
 
 export const handler = processAuctions
